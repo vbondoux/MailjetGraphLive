da310bdeaf6cdac1098b613d6044cb9a14c654d7
vbondoux <vbondoux@gmail.com>
2025-04-01 14:50:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import GraphPage from "./GraphPage";
+import React from "react";
 
 function App() {
-  return <GraphPage />;
+  return (
+    <div>
+      <h1>Mailjet Graph Live</h1>
+      <p>Frontend opérationnel.</p>
+    </div>
+  );
 }
 
 export default App;
