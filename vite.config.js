34c1ae5876ff95d9f11eeb7b3649f95d7f989c06
vbondoux <vbondoux@gmail.com>
2025-04-01 14:52:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 3000
-  },
-  preview: {
-    allowedHosts: ['mailjetgraphlive-production.up.railway.app'],
-    port: 4173
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
   }
-})
+});
